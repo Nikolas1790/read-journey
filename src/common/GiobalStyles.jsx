4fb3dd21b6f68a5303de014aff8a5bd232a35dc1
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-02-27 23:33:50 +0200

    connecting-pictures-collors-favicon-global_styled

---
+++ src/common/GiobalStyles.jsx
@@ -0,0 +1,92 @@
+import { createGlobalStyle } from 'styled-components';
+import color from './GlobalColers';
+
+// import GilroyBold from '../fonts/Gilroy-Bold.woff2';
+// import GilroyMedium from '../fonts/Gilroy-Medium.woff2';
+// import SFProTextSemibold from '../fonts/SFProText-Semibold.woff2';
+
+export const GlobalStyle = createGlobalStyle`
+
+/* @font-face {
+  font-family: 'Gilroy';
+  font-style: normal;
+  font-weight: 700; // Bold
+  
+}
+@font-face {
+  font-family: 'Gilroy';
+  font-style: normal;
+  font-weight: 500; // Medium
+  
+}
+@font-face {
+  font-family: 'SF Pro Text';
+  font-style: normal;
+  font-weight: 600; // Semibold
+  
+} */
+
+
+
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Gilroy", -apple-system, BlinkMacSystemFont, "SF Pro Text", 
+			"Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+			"Helvetica Neue", sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  font-style: normal;
+  line-height: 1.5;
+  color: ${color.blackPrimary};
+  width: 100%;
+  height: 100vh;
+  margin: 0;  
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+a {
+  text-decoration: none;
+  color: currentColor;
+}
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+button {
+  cursor: pointer;
+  padding: 0;
+  border: 0;
+  outline: 0;
+  font-family: inherit;
+}
+html {
+  scroll-behavior: smooth;
+}
+
+.no-scroll {
+  overflow: hidden;
+}
+
+`;
---
+++ src/common/GlobalColers.jsx
@@ -0,0 +1,16 @@
+const color = {
+  whitePrimary: '#F9F9F9',
+  gryeLight: '#686868',
+  gryeDark: 'rgba(#E3E3E3 /30%)',
+  gryeBlack: '#262626',
+  blackLight: '#1F1F1F', 
+  black: '#141414', 
+  green: '#30B94D',
+  blue: '#4F92F7',
+
+
+  greyDivider: 'rgba(18, 20, 23, 0.2)', 
+};
+  
+export default color;
+  
