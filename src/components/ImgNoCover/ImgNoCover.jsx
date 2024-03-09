58c18880d2b406936758da4fe1f988c6be18af88
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-03-09 21:00:10 +0200

    dell-comment-text

--- src/components/ImgNoCover/ImgNoCover.jsx
+++ src/components/ImgNoCover/ImgNoCover.jsx
@@ -1,12 +1,12 @@
 
-import desctopImg from '../../img/fon/desctop-block.jpg';
-import desctopImg2x from '../../img/fon/desctop-block@2x.jpg';
+// import desctopImg from '../../img/fon/desctop-block.jpg';
+// import desctopImg2x from '../../img/fon/desctop-block@2x.jpg';
 
-export default function ImgNoCover() {
-  return (    
-      <picture>
-        <source srcSet={desctopImg2x} media="(min-resolution: 192dpi)" />
-        <img src={desctopImg} alt="register img" />
-      </picture>  
-  );
-}
+// export default function ImgNoCover() {
+//   return (    
+//       <picture>
+//         <source srcSet={desctopImg2x} media="(min-resolution: 192dpi)" />
+//         <img src={desctopImg} alt="register img" />
+//       </picture>  
+//   );
+// }
